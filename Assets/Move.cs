31043ca6fd18ebc52a65bbb6a39ed823b1f48b71
pin <pin@pindeMacBook-Pro.local>
2018-05-08 21:25:52 +0800

    first commit

---
+++ Assets/Move.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate (new Vector3 (Input.GetAxis ("Horizontal"), 0, 0));
+		transform .Translate (new Vector3 (0,Input .GetAxis ("Vertical"),0));
+	}
+}
